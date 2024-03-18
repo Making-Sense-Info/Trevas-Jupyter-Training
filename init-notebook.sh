6f6a072343ecaf54d2c4c7238cc186e798a31a3e
Nicolas Laval <nicolas.laval@making-sense.info>
2024-03-18 10:32:52 +0100

    Init project

---
+++ init-notebook.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Clone the repository in /home/jovyan/work
+REPO=Trevas-Jupyter-Training
+REPO_URL=https://github.com/Making-Sense-Info/${REPO}.git
+git clone $REPO_URL $ROOT_PROJECT_DIRECTORY/${REPO}
+chown -R onyxia:users $ROOT_PROJECT_DIRECTORY/${REPO}
+
+# Open the relevant notebook when starting Jupyter Lab
+jupyter server --generate-config
+echo "c.LabApp.default_url = '/lab/tree/Trevas-Jupyter-Training/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
+echo "c.IPKernelApp.capture_fd_output = False" >> $HOME/.jupyter/jupyter_server_config.py
+echo "c.MultiKernelManager.default_kernel_name = 'trevas'" >> $HOME/.jupyter/jupyter_server_config.py
