19b4c735f6b5225700ad31ffab7e20b37df1a841
Nicolas Laval <nicolas.laval@making-sense.info>
2024-03-19 20:28:57 +0100

    Update init-notebook.sh

--- init-notebook.sh
+++ init-notebook.sh
@@ -10,3 +10,7 @@ jupyter server --generate-config
 echo "c.LabApp.default_url = '/lab/tree/Trevas-Jupyter-Training/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
 echo "c.IPKernelApp.capture_fd_output = False" >> $HOME/.jupyter/jupyter_server_config.py
 echo "c.MultiKernelManager.default_kernel_name = 'trevas'" >> $HOME/.jupyter/jupyter_server_config.py
+
+# Clean
+rm LICENSE
+rm init-notebook.sh
